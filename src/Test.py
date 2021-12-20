850cd2582b48fdeb071b7b3499508f583d832745
Sahar Tuvyahu <sahar45456@gmail.com>
2021-12-20 18:34:05 +0200

    Tests for Node, Edge and Digraph are done

---
+++ src/Test.py
@@ -0,0 +1,113 @@
+import unittest
+
+from Node import Node
+from Edge import Edge
+from DiGraph import DiGraph
+from GraphAlgo import GraphAlgo
+
+
+class NodeTestCase(unittest.TestCase):
+
+    def test_get_id(self):
+        node = Node()
+        self.assertEqual(node.get_id(), -1)
+        node.id = 4
+        self.assertEqual(node.get_id(), 4)
+
+    def test_add_in_edge(self):
+        node = Node()
+        self.assertEqual(node.get_in_edges(), {})
+        node.add_in_edge((5, node.get_id(), 3.314))
+        self.assertNotEqual(node.get_in_edges(), {})
+        self.assertTrue(len(node.get_in_edges()) == 1)
+
+    def test_add_out_edge(self):
+        node = Node()
+        self.assertEqual(node.get_out_edges(), {})
+        node.add_out_edge((node.get_id(), 5, 3.314))
+        self.assertNotEqual(node.get_out_edges(), {})
+        self.assertTrue(len(node.get_out_edges()) == 1)
+
+    def test_remove_in_edge(self):
+        node = Node()
+        node.add_in_edge((5, node.get_id(), 3.314))
+        node.add_in_edge((4, node.get_id(), 2.314))
+        self.assertTrue(len(node.in_edges) == 2)
+        self.assertEqual(node.remove_in_edge(5), True)
+        self.assertTrue(len(node.in_edges) == 1)
+        self.assertEqual(node.remove_in_edge(5), False)
+
+    def test_remove_out_edge(self):
+        node = Node()
+        node.add_out_edge((node.get_id(), 5, 3.314))
+        node.add_out_edge((node.get_id(), 4, 2.314))
+        self.assertTrue(len(node.out_edges) == 2)
+        self.assertEqual(node.remove_out_edge(5), True)
+        self.assertTrue(len(node.out_edges) == 1)
+        self.assertEqual(node.remove_out_edge(5), False)
+
+
+class DiGraphTestCase(unittest.TestCase):
+
+    def test_all_in_edges_of_node(self):
+        graph = DiGraph()
+        for n in range(4):
+            graph.add_node(n)
+        for n in range(1, 4):
+            graph.add_edge(n, 0, 1)
+        self.assertEqual(len(graph.all_in_edges_of_node(0)), 3)
+
+    def test_all_out_edges_of_node(self):
+        graph = DiGraph()
+        for n in range(4):
+            graph.add_node(n)
+        for n in range(1, 4):
+            graph.add_edge(0, n, 1)
+        self.assertEqual(len(graph.all_out_edges_of_node(0)), 3)
+
+    def test_add_edge(self):
+        graph = DiGraph()
+        for n in range(4):
+            graph.add_node(n)
+        graph.add_edge(0, 1, 1)
+        graph.add_edge(1, 0, 1.1)
+        self.assertTrue(graph.e_size() == 2)
+        self.assertTrue(len(graph.nodes[0].out_edges) == 1)
+        self.assertTrue(len(graph.nodes[0].in_edges) == 1)
+        self.assertTrue(len(graph.nodes[1].out_edges) == 1)
+        self.assertTrue(len(graph.nodes[1].in_edges) == 1)
+
+    def test_add_node(self):
+        graph = DiGraph()
+        self.assertTrue(graph.v_size() == 0)
+        for n in range(4):
+            graph.add_node(n)
+        self.assertTrue(graph.v_size() == 4)
+
+    def test_remove_node(self):
+        graph = DiGraph()
+        for n in range(2):
+            graph.add_node(n)
+        graph.add_edge(0, 1, 1)
+        graph.add_edge(1, 0, 1.1)
+        graph.remove_node(0)
+        self.assertTrue(graph.e_size() == 0)
+        self.assertTrue(len(graph.nodes[1].out_edges) == 0)
+        self.assertTrue(len(graph.nodes[1].in_edges) == 0)
+
+    def test_remove_edge(self):
+        graph = DiGraph()
+        for n in range(4):
+            graph.add_node(n)
+        graph.add_edge(0, 1, 1)
+        graph.add_edge(1, 0, 1.1)
+        graph.add_edge(1, 3, 1.9)
+        self.assertTrue(graph.e_size() == 3)
+        self.assertTrue(len(graph.nodes[1].get_out_edges()) == 2)
+        graph.remove_edge(1, 3)
+        self.assertTrue(graph.e_size() == 2)
+        self.assertTrue(len(graph.nodes[1].get_out_edges()) == 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
